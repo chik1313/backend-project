305ffad105f0e52ace4361e3efea3fc3351483ea
chik <ochkurov96@gmail.com>
2023-07-23 09:59:55 +0300

    comm

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
 const postRouter = require('./routes/project.routes')
-const cors = require("cors");
+
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
+
 app.use(express.json())
-app.use(cors({origin: '*'}))
+
 app.use('/api',userRouter)
 app.use('/api',postRouter)
+
+app.get('/', (req, res) => {
+    res.send('Hello Technogroup !');
+});
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`))
