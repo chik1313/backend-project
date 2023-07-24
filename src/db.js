5b7d4a0bfac6102948be65c8f6d2ed9448a36a20
chik <ochkurov96@gmail.com>
2023-07-24 16:32:35 +0300

    add changes

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "postgres",
-    password: "chik",
+  /*  user: "chik1313",
+    password: "UlvDHp29CTLA",
     host: "localhost",
     port: "5432",
-    database: "test_db"
+    database: "test_db"*/
+    connectionString: 'postgres://chik1313:UlvDHp29CTLA@ep-white-snowflake-820248.us-east-2.aws.neon.tech/neondb',
+    ssl : true
 })
 
 module.exports = pool
+//postgres://chik1313:UlvDHp29CTLA@ep-white-snowflake-820248.us-east-2.aws.neon.tech/neondb
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
 const postRouter = require('./routes/project.routes')
+const cors = require("cors");
 
 
 const PORT = process.env.PORT || 8080
@@ -9,6 +10,7 @@ const app = express()
 
 
 app.use(express.json())
+app.use(cors({origin: '*'}))
 
 app.use('/api',userRouter)
 app.use('/api',postRouter)
