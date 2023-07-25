2e7b9505da4401aec3e3509ea0d19d9491985e71
chik <ochkurov96@gmail.com>
2023-07-25 16:11:08 +0300

    add changes

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,16 +1,16 @@
 const db = require('../db')
 class ProjectController {
     async createProject(req, res) {
-        const {id} = req.body
-        let date = () => new Date().toLocaleDateString()
+        const {id,currentdate} = req.body
+        /*let date = () => new Date().toLocaleDateString()
         let newdate = date();
-        let currentdate = newdate.replace(/[^+\d]/g, '');
+        let currentdate = newdate.replace(/[^+\d]/g, '');*/
         const projects = await db.query(`SELECT * FROM project WHERE ID_DEP_CLIENT = $1 ORDER BY id_current_project DESC`, [id])
         let lastId = projects.rows.length > 0 ? projects.rows[0].id_current_project + 1 : 0
         const idProjectName = `${lastId+1}-${id}-${currentdate}`
         const newProject = await db.query(`INSERT INTO project (ID_DEP_CLIENT, ID_PROJECT, id_current_project ) values ($1, $2, $3) RETURNING *`, [id, idProjectName, lastId])
         res.json(newProject.rows[0])
-        console.log(currentdate)
+
     }
 
     async getProjectbyUser(req, res) {
